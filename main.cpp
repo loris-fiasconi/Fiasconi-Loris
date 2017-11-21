82b1e9cec3c9158c9fff60ea44a329f91286a077
fiasconiloris <loris599@hotmail.com>
2017-11-21 10:51:09 +0100

    seconda modifica CPP MASTER

--- main.cpp
+++ main.cpp
@@ -3,15 +3,16 @@
 using namespace std;
 int main()
 {
-  int n=5; //conflitto 
-  dfsdf
+  
+int z=5; //conflitto 
+  
   int c=0;
   for(int i=0;i<n;i++)
   {
-     c++;
+     z++;
   }
-cosdfsfsdf
-  cout <<c;
+
+  cout <<z;
 	 
 return 0;
 }
